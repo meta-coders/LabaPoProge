87d8d8c93993059cd3917317a6956e67dbbfed25
dimanadko <dimanadobko@gmail.com>
2018-04-03 16:19:32 +0300

    Initial Commit

---
+++ LabaPoProge/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LabaPoProge
+//
+//  Created by Dima on 03.04.18.
+//  Copyright © 2018 dimanadko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
